345b2aa0376aace3a5a45ba07037072b65e4e1df
kimhyunho <volume5522@naver.com>
2023-09-04 12:19:38 +0900

    거북이 움직여서 색깔 블랙홀 만들기

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import turtle
+colors=["red","purple","blue","green","yellow","orange"]
 t=turtle.Turtle()
 
-t.shape("turtle")
-t.forward(100)
-t.left(120)
-t.forward(100)
-t.left(120)
-t.forward(100)
+turtle.bgcolor("black")
+t.speed(0)
+t.width(3)
+length = 10
+
+while length<300:
+ t.forward(length)
+ t.pencolor(colors[length%6])
+ t.right(86)
+ length+=5
 
 turtle.mainloop()
-turtle.bye
+turtle.bye()
