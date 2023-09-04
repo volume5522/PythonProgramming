68ebf6229d2046bf8f7b6f10ff9a63b30b7cf98a
kimhyunho <volume5522@naver.com>
2023-09-04 12:43:58 +0900

    터틀 그래픽을 사용하여 반지름이 100 픽셀인 3개의 원을 그리는 프로그램

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-x=100
-y=200
-sum=x+y
-print("합은",sum)
+import turtle
+t=turtle.Turtle()
+t.shape("turtle")
+
+radius=100
+t.circle(radius)
+t.fd(30)
+t.circle(radius)
+t.fd(30)
+t.circle(radius)
+
+turtle.mainloop()
+turtle.bye()
